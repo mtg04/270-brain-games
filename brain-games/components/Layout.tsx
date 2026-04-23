38c3e4d6cf4a10f9669c2dd231818a43f1d1e2f0
mtg04 <mikeyg3001@gmail.com>
2026-04-23 06:55:20 -0400

    restyled wordle to match rest of the project - added score / restart game button

--- brain-games/components/Layout.tsx
+++ brain-games/components/Layout.tsx
@@ -6,7 +6,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div style={{ display: "flex", minHeight: "100vh", background: "#0f172a" }}>
       <Sidebar />
 
-      <main style={{ flex: 1, padding: "2rem" }}>{children}</main>
+      <main
+        style={{
+          flex: 1,
+          padding: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        {children}
+      </main>
     </div>
   );
 }
